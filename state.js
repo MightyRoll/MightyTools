26587d0daf03218e5f71359f08a1019502c25857
jppianta <joao.pianta@outlook.com>
2020-09-16 20:10:01 -0300

    Adapt state to allow multiple chat instances

--- state.js
+++ state.js
@@ -1,30 +1,88 @@
-const events = require('./events');
+const { MessageState, Message } = require('./messageState');
 
 class State {
-  #messages;
   #port;
 
   constructor() {
-    this.#messages = [];
+    this.messages = {
+      public: new MessageState('public'),
+      self: new MessageState('self')
+    };
     this.#port = 8080;
   }
 
-  addMessage(message) {
-    this.#messages.push(message);
-    events.launch('messageUpdate', this.#messages);
+  /**
+   * Adds a new message state if the channel does not yet exist
+   * @param {string} channelName 
+   */
+  addMessageState(channelName) {
+    if (!(channelName in this.messages)) {
+      this.messages[channelName] = new MessageState(channelName);
+    }
   }
 
-  getMessages() {
-    return this.#messages;
+  /**
+   * Add message to `destination` channel
+   * @param {Message} message 
+   * @param {string} destination 
+   */
+  addMessage(message, destination = 'public') {
+    if (destination in this.messages) {
+      this.messages[destination].addMessage(message);
+    } else {
+      // TODO: Throw error: Channel does not exist
+    }
   }
 
-  getLastMessages(numberOfMessages) {
-    return this.#messages.slice(numberOfMessages);
+  /**
+   * Add a list of messages to `destination` channel
+   * @param {Message[]} messages
+   * @param {string} destination 
+   */
+  addMessages(messages, destination = 'public') {
+    if (destination in this.messages) {
+      this.messages[destination].addMessages(messages);
+    } else {
+      // TODO: Throw error: Channel does not exist
+    }
   }
 
+  /**
+   * Get all messages from `destination`
+   * @param {string} destination
+   * @returns {Message[]}
+   */
+  getMessages(destination) {
+    if (destination in this.messages) {
+      this.messages[destination].getMessages();
+    } else {
+      // TODO: Throw error: Channel does not exist
+    }
+  }
+
+  /**
+   * Get last `numberOfMessages` messages from `destination`
+   * @param {number} numberOfMessages
+   * @param {string} destination
+   * @returns {Message[]}
+   */
+  getLastMessages(numberOfMessages, destination) {
+    if (destination in this.messages) {
+      this.messages[destination].getLastMessages(numberOfMessages);
+    } else {
+      // TODO: Throw error: Channel does not exist
+    }
+  }
+
+  /**
+   * Returns the port used by the server
+   * @returns {number}
+   */
   getPort() {
     return this.#port;
   }
 }
 
-module.exports = new State();
+module.exports = {
+  state: new State()
+};
